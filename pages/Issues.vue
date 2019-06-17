2a2aafa284b2debace6fb37fa95bcc0add7d7ed4
matthewFisher <m.r.f.48215@gmail.com>
2019-06-17 23:32:22 +0900

    basic boiler plate - axios working

---
+++ pages/Issues.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+      <div v-for="issue in issues" :key="issue.id">
+        <div v-for="item in issue" :key="item.id">
+          <h3>{{item.url}}</h3>
+        </div>
+      </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    async asyncData({ app }) {
+      const issues = await app.$axios.$get('https://api.github.com/search/issues?q=https://github.com/gatsbyjs/gatsby/');
+      console.log(await issues);
+      return { issues };
+    }
+  }
+</script>
